0ff17a36b57c27f4b41fc67f09e552eebb053e65
Armaghan <armaghan.sma@gmail.com>
2024-10-29 14:09:27 +0330

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # web-crawler
 
-This is a simple Python-based web crawler that extracts all the hyperlinks (`<a>` tags with `href` attributes) from a given 
-webpage and saves them in a CSV file.
+This Python script crawls a website up to a specified depth, extracting and displaying links in a graph structure. It uses `requests` to fetch pages and `BeautifulSoup` to parse and collect links recursively.
--- crawler.py
+++ crawler.py
@@ -1,37 +1,51 @@
-import csv
-
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
-def get_request(url):
-    response = requests.get(url)
-    return (response.status_code, response.text)
+def get_request(first_url):
+        response = requests.get(first_url)
+        return (response.status_code, response.text)
 
 
-def extract_link(html):
+def extract_link(html, baseurl):
     soup = BeautifulSoup(html, 'lxml')
     links = []
 
     for link in soup.find_all("a", href=True):
-        links.append(link)
+        href = link['href']
+        full_url = urljoin(baseurl, href)
+        if full_url.startswith('http'):
+            links.append(full_url)
 
-    return links
+    return list(set(links))
 
 
-def save_link_to_csv(links):
-    with open("url_links.csv", 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(['links'])
+def graph(first_url, max_depth):
+    link_graph = {}
+    visited_url = set()
+
+    def crawl(url, depth):
+        if depth > max_depth or url in visited_url:
+            return
+
+        visited_url.add(url)
+        html_content = get_request(url)
+
+        if html_content is None:
+            return
+
+        links = extract_link(html_content[1], url)
+        link_graph[url] = links
+
         for link in links:
-            writer.writerow([link])
+            crawl(link, depth+1)
 
 
-def scrape_links(page_url):
-    html_content = get_request(page_url)
-    links = extract_link(html_content[1])
-    save_link_to_csv(links)
+    crawl(first_url, 0)
+    return link_graph
 
 
-url = input()
-scrape_links(url)
+first_url = input("url: ")
+max_depth = input("max_depth: ")
+print(graph(first_url, max_depth))
